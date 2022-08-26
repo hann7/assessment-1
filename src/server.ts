2f977a05e9e66ad2cdb8b11f2e30b916d5e81125
Hannah Santoyo <hannahrichardxo@gmail.com>
2022-08-25 23:15:41 -0700

    created base file structure, more boilerplate, basic express server code

---
+++ src/server.ts
@@ -0,0 +1,24 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const carController = require('./controllers/carController')
+
+app.use(express.json());
+
+app.use('/cars', carController)
+
+// global error handler
+app.use((err, req, res, next) => {
+  const defaultErr = {
+    log: 'Express error handler caught unknown middleware error',
+    status: 500,
+    message: { err: 'An error occurred' },
+  };
+  const errorObj = Object.assign({}, defaultErr, err);
+  console.log(errorObj.log);
+  return res.status(errorObj.status).json(errorObj.message);
+});
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}`);
+});
